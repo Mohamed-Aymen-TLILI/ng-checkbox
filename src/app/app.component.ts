7fa774c77b859dcf1fb1a45859d3521f878c07da
libra1919 <aymentli@gmail.com>
2020-12-07 22:47:13 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
+import { FormGroup, FormArray, FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-checkbox';
+  items = [
+    { name: 'One', id: 1, group: 'A' },
+    { name: 'Two', id: 2, group: 'A' },
+    { name: 'Three', id: 3, group: 'A' },
+    { name: 'Four', id: 4, group: 'B' },
+    { name: 'Five', id: 5, group: 'B' },
+    { name: 'Six', id: 6, group: 'B' },
+  ]
+
+  form = new FormGroup({
+    items: new FormArray([])
+  })
+
+  ngOnInit() {
+    this.items.forEach(e => {
+      this.control.push(new FormControl(false));
+    })
+  }
+
+  submit() { }
+
+  get control() {
+    return this.form.get('items') as FormArray;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TreePipe } from './tree.pipe';
+import { CheckChildrenDirective } from './check-children.directive';
+import { SelectGroupDirective } from './select-group.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    TreePipe,
+    CheckChildrenDirective,
+    SelectGroupDirective
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
